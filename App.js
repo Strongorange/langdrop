33807beaf1f608ed572c239cd84be52a8f80544c
Chanhwi Lee <chanhwi.lee6@gmail.com>
2022-08-09 23:33:43 +0900

    시작

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import styled from "styled-components/native";
+
+const View = styled.View`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Text = styled.Text``;
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
